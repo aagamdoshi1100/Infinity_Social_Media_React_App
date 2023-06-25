89fdb436d04d272d886a6b00ada7e84d2ce3326d
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-06-26 01:24:02 +0530

    added url and bio icon

--- src/contexts/IconContext.js
+++ src/contexts/IconContext.js
@@ -1,9 +1,8 @@
 import { createContext, useContext } from "react";
 import useUserFeedContext from "./UserFeedContext";
-import {BiArrowBack,BiLogOut,BiBookBookmark} from "react-icons/bi"
+import {BiArrowBack,BiLogOut,BiBookBookmark,BiLogIn,BiLink,BiGlobe} from "react-icons/bi"
 import {MdOutlineExplore} from "react-icons/md"
 import {AiOutlineHome} from "react-icons/ai"
-import {BiLogIn} from "react-icons/bi"
 
 const IconContext = createContext();
 
@@ -21,7 +20,7 @@ export default function IconContextProvider({children}){
         userFeedDispacher({type:"EXPLORE_PAGE",payload:"postsData"})
         navigate('/pages/explore/Explore')
         }
-    return(<IconContext.Provider value={{goToBookMark,goToHome,goToExplore,BiArrowBack,BiLogOut,BiBookBookmark,MdOutlineExplore,AiOutlineHome,BiLogIn}}>{children}</IconContext.Provider>)
+    return(<IconContext.Provider value={{goToBookMark,goToHome,goToExplore,BiArrowBack,BiLogOut,BiBookBookmark,MdOutlineExplore,AiOutlineHome,BiLogIn,BiLink,BiGlobe}}>{children}</IconContext.Provider>)
 }
 
 export const useIconContext =()=>useContext(IconContext)
