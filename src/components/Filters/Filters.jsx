f37a39fe96d68a95f006a10118b8c021351131bb
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-06-27 20:48:17 +0530

    refactor:Moved from user feed and created separate component

---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,22 @@
+import { useIconContext } from "../../contexts/IconContext";
+import useUserFeedContext from "../../contexts/UserFeedContext"
+
+export default function Filters(){
+    const {userFeed,userFeedDispacher} = useUserFeedContext();
+    const {FaFilter} = useIconContext();
+    return(
+        <div className="filterBox">
+        <p className="heading-filterbox">{userFeed.filterBy}</p>
+        <FaFilter className="filterBox-icon" onClick={()=>userFeedDispacher({type: "SHOW_FILTERS",payload: userFeed.showFiltersUserFeed})}/>
+      
+    {userFeed.showFiltersUserFeed ? 
+        <div className="filterBox-types">
+                <span onClick={()=>userFeedDispacher({type : "SORT_BY_TRENDING", payload: [...userFeed.postsData] })}>Trending</span>
+                <p onClick={()=>userFeedDispacher({type : "SORT_BY_LATEST", payload: [...userFeed.postsData] })}>Latest</p>
+                <span onClick={()=>userFeedDispacher({type : "SORT_BY_OLDEST", payload: [...userFeed.postsData]})}>Oldest</span>
+            
+        </div> : null
+    }
+    </div>
+    )
+}
