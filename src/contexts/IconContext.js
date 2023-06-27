5e70f77a5576b74c738219d8d8feaf0c0b8dbe98
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-06-27 20:49:24 +0530

    Added icons

--- src/contexts/IconContext.js
+++ src/contexts/IconContext.js
@@ -5,6 +5,8 @@ import {MdOutlineExplore,MdInsertPhoto} from "react-icons/md"
 import {AiOutlineHome,AiOutlineLike} from "react-icons/ai"
 import {FiBookmark} from "react-icons/fi"
 import {GoComment} from "react-icons/go"
+import {FaFilter} from "react-icons/fa"
+import {SlUserFollow} from "react-icons/sl"
 
 const IconContext = createContext();
 
@@ -22,7 +24,7 @@ export default function IconContextProvider({children}){
         userFeedDispacher({type:"EXPLORE_PAGE",payload:"postsData"})
         navigate('/pages/explore/Explore')
         }
-    return(<IconContext.Provider value={{goToBookMark,goToHome,goToExplore,BiArrowBack,BiLogOut,BiBookBookmark,MdOutlineExplore,AiOutlineHome,BiLogIn,BiLink,BiGlobe,BiDotsVertical,MdInsertPhoto,AiOutlineLike,FiBookmark,GoComment}}>{children}</IconContext.Provider>)
+    return(<IconContext.Provider value={{goToBookMark,goToHome,goToExplore,BiArrowBack,BiLogOut,BiBookBookmark,MdOutlineExplore,AiOutlineHome,BiLogIn,BiLink,BiGlobe,BiDotsVertical,MdInsertPhoto,AiOutlineLike,FiBookmark,GoComment,FaFilter,SlUserFollow}}>{children}</IconContext.Provider>)
 }
 
 export const useIconContext =()=>useContext(IconContext)
