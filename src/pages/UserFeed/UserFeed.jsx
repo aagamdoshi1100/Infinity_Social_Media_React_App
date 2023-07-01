b3103a4a5bfcadd5afd96f1c5ff8bfdc1e6a866e
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-02 01:16:58 +0530

    refactor: Created components NewPost and AllUsers from UserFeed

--- src/pages/UserFeed/UserFeed.jsx
+++ src/pages/UserFeed/UserFeed.jsx
@@ -8,6 +8,8 @@ import Footer from "../../components/Footer/Footer"
 import Heading from "../../components/Header/Heading"
 import Filters from "../../components/Filters/Filters"
 import { useIconContext } from "../../contexts/IconContext"
+import AllUsers from "../../components/AllUsers/AllUsers"
+import NewPost from "../../components/NewPost/NewPost"
 
  
 export default function UserFeed(){
@@ -21,47 +23,16 @@ export default function UserFeed(){
     return(<div className="container">
     <Heading />
     <div className="container-body">
-    <div className="create-post">
-            <textarea className="myText" onChange={(e)=>userFeedDispacher({type:"CREATE_POST_CONTENT",payload:e.target.value})} placeholder="Write something..." ></textarea>
-        <div className="create-post-controls">
-            <div className="image-box">
-                <img src={userFeed.createPostImage === null || userFeed.createPostImage === undefined  ? "" : `${URL.createObjectURL(userFeed.createPostImage)}` } onClick={()=>userFeedDispacher({type:"PREVIEW",payload: userFeed.previewUploadedImage})}/>
-            </div>
-            <div className="btn-imgpicker">
-                <label htmlFor="image">
-                        <MdInsertPhoto size="1.9em"/>
-                </label>
-                <input type="file" id="image" style={{display:"none",visibility:"none"}} onChange={(e)=>userFeedDispacher({type:"CREATE_POST_IMAGE",payload:e.target.files[0]})}/>
-                <button className="btn-post" onClick={createPost}>Create Post</button>
-            </div>
-            {userFeed.previewUploadedImage ? 
-            <div className="preview-post b br">
-                <h3>Post preview</h3>
-                <p>{userFeed?.createPostContent}</p>
-                <img src={userFeed.createPostImage === null || userFeed.createPostImage === undefined  ? "" : `${URL.createObjectURL(userFeed.createPostImage)}` } />
-                <button onClick={createPost}>Post</button>
-                <button onClick={()=>userFeedDispacher({type:"PREVIEW",payload: userFeed.previewUploadedImage})}>Discard</button>
-            </div> : null 
-            }
-        </div>
+    <div className="create-post-div">
+     <NewPost />
     </div>
 
-    <div className="allUsers">
-        {infinityUsers?.allUsers.filter(({username})=>username !== user.name).map((details)=>{
-            const {_id,profileIcon,firstName,lastName,username} = details;
-            return(<div className="userBox" key={_id}>
-                <span className="circle"><img src={profileIcon} width="100%" height="100%"/></span>
-                <h4>{`${firstName} ${lastName}`}</h4>
-                <p className="username">{`@${username}`}</p>
-            {
-                infinityUsers?.followedByLoggedInUser.find((item)=>item.username ===username) ? <button className="btn br" onClick={()=>followUser(_id)}>UnFollow</button>  : <button className="btn br" onClick={()=>followUser(_id)}>Follow</button> 
-            }
-            </div>)
-        })}
+    <div className="allUsers-div">
+     <AllUsers />       
     </div>
         
         {infinityUsers.followUsers.length > 0 ? 
-        <div>
+        <div className="posts-div"> 
         <Filters />
         <FetchData /> 
         </div>
