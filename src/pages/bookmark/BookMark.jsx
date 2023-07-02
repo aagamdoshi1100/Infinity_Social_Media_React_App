a5b70330d175860e097b19c086cbeac2713397be
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-03 01:32:39 +0530

    style:bookmark

--- src/pages/bookmark/BookMark.jsx
+++ src/pages/bookmark/BookMark.jsx
@@ -2,12 +2,15 @@ import FetchData from "../../components/FetchData/FetchData";
 import Footer from "../../components/Footer/Footer";
 import { useIconContext } from "../../contexts/IconContext";
 import {BiArrowBack} from "react-icons/bi"
+import "./BookMark.css"
 
 export default function BookMark(){
     const {goToHome} = useIconContext()
     return(<div>
-        <BiArrowBack size="1.7em" onClick={goToHome}/>
+        <div className="bookMark-header"><BiArrowBack size="1.7em" onClick={goToHome}/><h2>BookMark</h2></div>
+        <div className="container">
         <FetchData />
+        </div>
         <Footer />
     </div>)
 }
