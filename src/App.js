d3e96410d261700ae47f0594a1fd53ba8c292548
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-17 03:34:07 +0530

    feat:protected routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import './App.css';
-import { Routes,Route } from 'react-router-dom';
-import UserFeed from './pages/UserFeed/UserFeed';
-import SinglePostView from './pages/SinglePostView/SinglePostView';
-import UserProfile from './pages/profile/UserProfile';
-import Explore from './pages/explore/Explore';
-import BookMark from './pages/bookmark/BookMark';
-import Login from './pages/Authentication/Login';
-import SignUp from './pages/Authentication/SignUp';
-import RequiresAuth from './components/RequiresAuth';
-import { ToastContainer } from "react-toastify"; 
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import UserFeed from "./pages/UserFeed/UserFeed";
+import SinglePostView from "./pages/SinglePostView/SinglePostView";
+import UserProfile from "./pages/profile/UserProfile";
+import Explore from "./pages/explore/Explore";
+import BookMark from "./pages/bookmark/BookMark";
+import Login from "./pages/Authentication/Login";
+import SignUp from "./pages/Authentication/SignUp";
+import RequiresAuth from "./components/RequiresAuth";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Avtar from './pages/Avtar/Avtar';
+import Avtar from "./pages/Avtar/Avtar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header"></header>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/pages/Authentication/SignUp" element={<SignUp />} />
         <Route path="/pages/Avtar/Avtar" element={<Avtar />} />
-        <Route path="/pages/UserFeed/UserFeed" element={ <UserFeed /> }/>
-        <Route path="/pages/SinglePostView/" element={<SinglePostView />}/>
-        <Route path="/pages/profile/UserProfile" element={<UserProfile />} />
-        <Route path="/pages/explore/Explore" element={<Explore />} />
-        <Route path="/pages/bookmark/BookMark" element={<BookMark />} />
+        <Route
+          path="/pages/UserFeed/UserFeed"
+          element={
+            <RequiresAuth>
+              <UserFeed />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/pages/SinglePostView/"
+          element={
+            <RequiresAuth>
+              <SinglePostView />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/pages/profile/UserProfile"
+          element={
+            <RequiresAuth>
+              <UserProfile />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/pages/explore/Explore"
+          element={
+            <RequiresAuth>
+              <Explore />
+            </RequiresAuth>
+          }
+        />
+        <Route
+          path="/pages/bookmark/BookMark"
+          element={
+            <RequiresAuth>
+              <BookMark />
+            </RequiresAuth>
+          }
+        />
       </Routes>
 
       <ToastContainer autoClose={2000} theme="colored" position="bottom-left" />
